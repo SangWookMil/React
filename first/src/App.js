bd2a86a0e5e063f2de9e70dec32224be1e8dca3a
SangWookMil <sangwook520@naver.com>
2022-02-27 14:45:57 +0000

    started project

--- first/src/App.js
+++ first/src/App.js
@@ -1,25 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
+  const [hello, sethello] = useState("hi");
+  var [addModal, setAddModal] = useState(false);
+
+  function handleAdd() {
+    console.log("clicked");
+    setAddModal(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Typical Todo</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Add</Nav.Link>
+            <Nav.Link href="#pricing">Delete</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {addModal ? <AddModal /> : undefined}
+      <AddButton onClick={handleAdd}/>
     </div>
   );
 }
+
+function AddButton() {
+  return (
+    <div className="add-button noselect">
+      <h2>+</h2>
+    </div>
+  );
+}
+
+function AddModal() {
+  return <div>hello</div>;
+}
+
+function Todo() {}
 
 export default App;
