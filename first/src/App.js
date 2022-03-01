d42805cc488452f21de84e055ee9b44396001a67
SangWookMil <sangwook520@naver.com>
2022-03-01 14:33:09 +0000

    20220301 update

--- first/src/App.js
+++ first/src/App.js
@@ -1,16 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  CloseButton,
+  OverlayTrigger,
+  Tooltip,
+  FloatingLabel,
+  Form,
+} from "react-bootstrap";
 
 function App() {
   const [hello, sethello] = useState("hi");
   var [addModal, setAddModal] = useState(false);
-
-  function handleAdd() {
-    console.log("clicked");
-    setAddModal(true);
-  }
 
   return (
     <div className="App">
@@ -24,22 +28,52 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      {addModal ? <AddModal /> : undefined}
-      <AddButton onClick={handleAdd}/>
+      {addModal ? <AddModal setAddModal={setAddModal} /> : undefined}
+      <AddButton addModal={addModal} setAddModal={setAddModal} />
     </div>
   );
 }
 
-function AddButton() {
+function AddButton(props) {
   return (
-    <div className="add-button noselect">
+    <div
+      className="add-button noselect"
+      onClick={() => {
+        props.setAddModal(true);
+      }}
+    >
       <h2>+</h2>
     </div>
   );
 }
 
-function AddModal() {
-  return <div>hello</div>;
+function AddModal(props) {
+  function closeModal() {
+    props.setAddModal(false);
+  }
+
+  return (
+    <div className="new-todo-background">
+      <div className="new-todo" tabIndex={"0"}>
+        <OverlayTrigger
+          overlay={<Tooltip id={`tooltip-top`}>Click to Cose.</Tooltip>}
+        >
+          <CloseButton className="close-button" onClick={closeModal} />
+        </OverlayTrigger>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-3"
+        >
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password">
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+      </div>
+    </div>
+  );
 }
 
 function Todo() {}
